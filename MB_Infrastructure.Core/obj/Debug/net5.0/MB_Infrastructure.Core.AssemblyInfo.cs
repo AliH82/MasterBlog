3bb9e21f6356c4bb15200ccc23b14822b7a81895
ali heydari <alirezaheydari.site@gmail.com>
2024-09-04 20:17:58 +0330

    commited

--- MB_Infrastructure.Core/obj/Debug/net5.0/MB_Infrastructure.Core.AssemblyInfo.cs
+++ MB_Infrastructure.Core/obj/Debug/net5.0/MB_Infrastructure.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MB_Infrastructure.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98e0974aa9bc30b962ed2c812c28816beec9e36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MB_Infrastructure.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MB_Infrastructure.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
